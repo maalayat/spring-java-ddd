c3645caf2de5887020ad258f9f7321fb11b79fee
Alejandro Ayala <mayalaat@pichincha.com>
2022-06-09 09:52:29 -0500

    Make out @Service domain annotation pure

--- app/main/ec/solmedia/app/Starter.java
+++ app/main/ec/solmedia/app/Starter.java
@@ -2,16 +2,20 @@ package ec.solmedia.app;
 
 import ec.solmedia.mooc.courses.infrastructure.entities.CourseEntity;
 import ec.solmedia.mooc.courses.infrastructure.persistence.CourseRepositoryImpl;
+import ec.solmedia.shared.domain.Service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 @EnableJpaRepositories(basePackageClasses = CourseRepositoryImpl.class)
 @EntityScan(basePackageClasses = CourseEntity.class)
-@ComponentScan({"ec.solmedia.app", "ec.solmedia.mooc", "ec.solmedia.backoffice"})
+@ComponentScan(
+    includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Service.class),
+    value = {"ec.solmedia.app", "ec.solmedia.mooc", "ec.solmedia.backoffice"})
 public class Starter {
 
   public static void main(String[] args) {
