d832b1e583a0a1baa3925fd1f5fba6645f44552c
Alejandro Ayala <mayalaat@pichincha.com>
2022-06-09 08:51:02 -0500

    Adding H2 database integration test

--- src/mooc/test/ec/solmedia/mooc/courses/infrastructure/persistence/InMemoryCourseRepositoryTest.java
+++ src/mooc/test/ec/solmedia/mooc/courses/infrastructure/persistence/InMemoryCourseRepositoryTest.java
@@ -3,18 +3,27 @@ package ec.solmedia.mooc.courses.infrastructure.persistence;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
 
+import ec.solmedia.app.Starter;
 import ec.solmedia.mooc.courses.domain.CourseIdMother;
 import ec.solmedia.mooc.courses.domain.CourseMother;
+import ec.solmedia.mooc.courses.domain.CourseRepository;
 import java.util.Optional;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.ContextConfiguration;
 
+@DataJpaTest
+@ContextConfiguration(classes = Starter.class)
 class InMemoryCourseRepositoryTest {
+
+  @Autowired
+  private CourseRepository repository;
 
   @Test
   @DisplayName("Save a course")
   void saveCourse() {
-    final var repository = new InMemoryCourseRepository();
     final var course = CourseMother.random();
 
     repository.save(course);
@@ -23,7 +32,6 @@ class InMemoryCourseRepositoryTest {
   @Test
   @DisplayName("Return an existing course")
   void returnExistingCourse() {
-    final var repository = new InMemoryCourseRepository();
     final var course = CourseMother.random();
 
     repository.save(course);
@@ -34,8 +42,6 @@ class InMemoryCourseRepositoryTest {
   @Test
   @DisplayName("Not return a non existing course")
   void notReturnNonExistingCourse() {
-    final var repository = new InMemoryCourseRepository();
-
     assertFalse(repository.search(CourseIdMother.random()).isPresent());
   }
 }
