183d48425e702f77d66b1ce43f0aed2af8765ded
Alejandro Ayala <mayalaat@pichincha.com>
2022-06-09 09:07:32 -0500

    Switch to "mocks" with annotations to CourseCreatorTest

--- src/mooc/test/ec/solmedia/mooc/courses/application/create/CourseCreatorTest.java
+++ src/mooc/test/ec/solmedia/mooc/courses/application/create/CourseCreatorTest.java
@@ -1,22 +1,29 @@
 package ec.solmedia.mooc.courses.application.create;
 
 import static org.mockito.Mockito.atLeastOnce;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 
 import ec.solmedia.mooc.courses.domain.CourseMother;
 import ec.solmedia.mooc.courses.domain.CourseRepository;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+@ExtendWith(MockitoExtension.class)
 class CourseCreatorTest {
+
+  @Mock
+  private CourseRepository repository;
+
+  @InjectMocks
+  private CourseCreator creator;
 
   @Test
   @DisplayName("Save a valid course")
   void saveValidCourse() {
-    final var repository = mock(CourseRepository.class);
-    final var creator = new CourseCreator(repository);
-
     final var request = CourseCreateRequestMother.random();
     final var course = CourseMother.fromRequest(request);
 
