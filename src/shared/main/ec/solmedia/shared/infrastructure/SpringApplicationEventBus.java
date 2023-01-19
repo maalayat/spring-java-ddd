02b944239d6814a188e24475b9cb2a4a465c3caa
Alejandro Ayala <mayalaat@pichincha.com>
2023-01-18 22:54:54 -0500

    Adding event bus infrastructure

---
+++ src/shared/main/ec/solmedia/shared/infrastructure/SpringApplicationEventBus.java
@@ -0,0 +1,21 @@
+package ec.solmedia.shared.infrastructure;
+
+import ec.solmedia.shared.domain.event.bus.DomainEvent;
+import ec.solmedia.shared.domain.event.bus.EventBus;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class SpringApplicationEventBus implements EventBus {
+
+  private final ApplicationEventPublisher publisher;
+
+  @Override
+  public void publish(List<DomainEvent> events) {
+    events.forEach(publisher::publishEvent);
+  }
+
+}
