2628ab9339e94cdc1eb7cc48614ba2a82f18a50e
Alejandro Ayala <mayalaat@pichincha.com>
2023-01-19 17:02:59 -0500

    Fixing starter configuration

--- app/main/ec/solmedia/app/Starter.java
+++ app/main/ec/solmedia/app/Starter.java
@@ -2,6 +2,8 @@ package ec.solmedia.app;
 
 import ec.solmedia.mooc.courses.infrastructure.entities.CourseEntity;
 import ec.solmedia.mooc.courses.infrastructure.persistence.CourseRepositoryImpl;
+import ec.solmedia.mooc.courses_counter.infrastructure.entities.CoursesCounterEntity;
+import ec.solmedia.mooc.courses_counter.infrastructure.persistence.CourseCounterRepositoryImpl;
 import ec.solmedia.shared.domain.Service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,11 +13,12 @@ import org.springframework.context.annotation.FilterType;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories(basePackageClasses = CourseRepositoryImpl.class)
-@EntityScan(basePackageClasses = CourseEntity.class)
+@EnableJpaRepositories(basePackageClasses =
+    {CourseRepositoryImpl.class, CourseCounterRepositoryImpl.class})
+@EntityScan(basePackageClasses = {CourseEntity.class, CoursesCounterEntity.class})
 @ComponentScan(
     includeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Service.class),
-    value = {"ec.solmedia.app", "ec.solmedia.mooc", "ec.solmedia.backoffice"})
+    value = {"ec.solmedia.app", "ec.solmedia.mooc", "ec.solmedia.backoffice", "ec.solmedia.shared"})
 public class Starter {
 
   public static void main(String[] args) {
